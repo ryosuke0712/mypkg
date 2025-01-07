6a15c231dc141231fb142d94da797290d9370c40
Ryosuke Kambara <rosery030517@gmail.com>
2025-01-07 12:01:07 +0900

    Add a command

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
 
      talker = launch_ros.actions.Node(
          package='mypkg',      #パッケージの名前を指定
-         executable='talker',  #実行するファイルの指定
+         executable='powerwatch',  #実行するファイルの指定
          )
      listener = launch_ros.actions.Node(
          package='mypkg',
