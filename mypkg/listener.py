169905e639761ef10a7e558670d9de34af80f614
Ryosuke Kambara <rosery030517@gmail.com>
2025-01-09 13:55:10 +0900

    Add a command

---
+++ mypkg/listener.py
@@ -0,0 +1,29 @@
+#SPDX-FileCopyrightText: 2025 Ryosuke Kambara
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class BatteryStatusListener(Node):
+    def __init__(self):
+        super().__init__('battery_status_listener')
+        self.subscription = self.create_subscription(
+            String,
+            'battery_status',
+            self.listener_callback,
+            10
+        )
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'Received message: {msg.data}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = BatteryStatusListener()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
